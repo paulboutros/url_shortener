3727360d88836c272f4625c1f15bffd085e4eb6b
paulboutros <paulboutros436@hotmail.com>
2025-05-28 09:44:54 +0100

    url_shortener_ comit 1

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,25 @@
+// app/[slug]/page.tsx
+import { redirect } from 'next/navigation'
+
+import rawLinks from '../../links.json'
+const links: { [key: string]: string } = rawLinks
+
+
+interface Params {
+  params: {
+    slug: string
+  }
+}
+
+export default function ShortUrlRedirect({ params }: Params) {
+  const { slug } = params
+  const destination = links[slug]
+
+  if (!destination) {
+    // If slug not found, show 404 page
+    return <h1>404 - Link not found</h1>
+  }
+
+  // Redirect immediately to the destination URL
+  redirect(destination)
+}
