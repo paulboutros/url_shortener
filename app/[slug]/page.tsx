472a3eeae0ba38e268ee7a23ec1938042ce70ae3
paulboutros <paulboutros436@hotmail.com>
2025-05-28 11:41:46 +0100

    jsonLink

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,4 +1,28 @@
 import { redirect } from 'next/navigation'
+import rawLinks from '../../links.json' // adjust path if needed
+
+const links: { [key: string]: string } = rawLinks
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+
+  const destination = links[slug]
+
+  if (destination) {
+    redirect(destination)
+  } else {
+    return <h1>404 - Link not found</h1>
+  }
+}
+
+
+
+/*
+import { redirect } from 'next/navigation'
 
 export default async function Page({
   params,
@@ -13,3 +37,4 @@ export default async function Page({
     return <h1>404 - Link not found</h1>
   }
 }
+*/
