66d9336b67e190114ccfbbb46f5efa294637cdf4
paulboutros <paulboutros436@hotmail.com>
2025-05-28 10:49:30 +0100

    aaaaaaaaaaa

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,41 +1,15 @@
-
-
 import { redirect } from 'next/navigation'
 
-export default function Page({ params }: { params: { slug: string } }) {
-  const slug = params.slug
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
 
-  // Simple redirect to example.com or 404 fallback
   if (slug === 'test') {
     redirect('https://example.com')
   } else {
     return <h1>404 - Link not found</h1>
   }
 }
-
-
-/*
-import { redirect } from 'next/navigation'
-import rawLinks from '../../links.json'
-
-const links: { [key: string]: string } = rawLinks
-
- interface PageProps {
-  params: {
-    slug: string
-  }
-}
-
-export default async function ShortUrlRedirect({ params }: PageProps) {
-  const { slug } = params
-  const destination = links[slug]
-
-  if (!destination) {
-    // If slug not found, show 404 page
-    return <h1>404 - Link not found</h1>
-  }
-
-  // Redirect immediately to the destination URL
-  redirect(destination)
-}
-*/
